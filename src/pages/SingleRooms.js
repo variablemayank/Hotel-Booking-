807b8dbae5221fb5a42120f1650dc7026709ed43
Mayank Sharma <mayank202020@rediffmail.com>
2019-08-12 20:54:01 +0530

    added files

---
+++ src/pages/SingleRooms.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SIngleRooms extends Component {
+    render() {
+        return (
+            <div>
+                hello from single rooms page
+            </div>
+        )
+    }
+}
